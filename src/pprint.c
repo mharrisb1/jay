33346b1936dbcc6ae3ac619f4ef2d210be4c22de
Michael Harris <mharris@luabase.com>
2024-10-04 11:33:37 -0400

    fet(pprint): make nulls black

--- src/pprint.c
+++ src/pprint.c
@@ -2,10 +2,11 @@
 
 #include "json.h"
 
-#define COLOR_BLUE         "\033[0;34m"
-#define COLOR_YELLOW_BOLD  "\033[1;33m"
-#define COLOR_MAGENTA_BOLD "\033[1;35m"
-#define COLOR_RESET        "\033[0m"
+#define COLOR_BLUE_BOLD "\033[1;34m"
+#define COLOR_YELLOW    "\033[0;33m"
+#define COLOR_MAGENTA   "\033[0;35m"
+#define COLOR_BLACK     "\033[0;30m"
+#define COLOR_RESET     "\033[0m"
 
 void pprint(const JsonNode *node, int indent) {
   if (!node)
@@ -13,17 +14,17 @@ void pprint(const JsonNode *node, int indent) {
 
   switch (node->type) {
     case JSON_NULL:
-      printf(COLOR_YELLOW_BOLD "null" COLOR_RESET);
+      printf(COLOR_BLACK "null" COLOR_RESET);
       break;
     case JSON_BOOL:
-      printf(COLOR_YELLOW_BOLD "%s" COLOR_RESET,
+      printf(COLOR_YELLOW "%s" COLOR_RESET,
              node->data.bool_value ? "true" : "false");
       break;
     case JSON_NUMBER:
-      printf(COLOR_YELLOW_BOLD "%lf" COLOR_RESET, node->data.number_value);
+      printf(COLOR_YELLOW "%lf" COLOR_RESET, node->data.number_value);
       break;
     case JSON_STRING:
-      printf(COLOR_MAGENTA_BOLD "\"%s\"" COLOR_RESET, node->data.string_value);
+      printf(COLOR_MAGENTA "\"%s\"" COLOR_RESET, node->data.string_value);
       break;
     case JSON_ARRAY: {
       printf("[\n");
@@ -52,7 +53,7 @@ void pprint(const JsonNode *node, int indent) {
         JsonKeyValuePair *pair = (JsonKeyValuePair *)current->node;
         for (int i = 0; i < indent + 1; i++)
           printf("  ");
-        printf(COLOR_BLUE "\"%s\"" COLOR_RESET ": ", pair->key);
+        printf(COLOR_BLUE_BOLD "\"%s\"" COLOR_RESET ": ", pair->key);
         pprint(pair->value, indent + 1);
 
         if (current->next)
